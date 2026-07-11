cbf428bb513cfd601f3e9e9c753c63b17af4eb03
MANOJ KUMAR PATEL <manojkpatel1308@gmail.com>
2026-07-11 07:26:44 +0530

    feat: explore simple web socket concepts and socket.io library

---
+++ 11_webSocket/index.js
@@ -0,0 +1,25 @@
+import http from "http";
+import express from "express";
+import path from "path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//Socket.io
+io.on('connection', (socket) => {
+    socket.on('user-message', (message) => {
+        io.emit("message", message);
+    })
+    console.log('A new user has connected', socket.id)
+})
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', (req, res) => {
+    return res.sendFile("")
+})
+server.listen(9000, () => {
+    console.log(`Server started at 9000`);
+})
